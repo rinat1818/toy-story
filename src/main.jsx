456feb4507d6cbb9fc5a80acfc1560949a04ee9c
Rinat <ladyubaitours@gmail.com>
2026-05-10 19:25:50 +0400

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+const elRoot =document.getElementById('root')
+createRoot(elRoot).render(<App />)
+
